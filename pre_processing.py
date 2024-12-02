c1f4532d67d6127e4b3e12c97d154c8fbe10f748
jwoods013 <108284104+jwoods013@users.noreply.github.com>
2024-12-02 12:14:53 -0800

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -2,6 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
@@ -14,9 +18,6 @@ y = dataset.iloc[:, -1].values
 print(x)
 print(y)
 
-# setup sklearn
-from sklearn.impute import SimpleImputer
-
 # replace missing values with mean
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(x[:, 1:3])
@@ -24,3 +25,16 @@ x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 # print values after replacing missing values
 print(x)
+
+#encoding independant using onehotencoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(),[0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+print(x)
+
+#encoding dependant variable
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(y)
